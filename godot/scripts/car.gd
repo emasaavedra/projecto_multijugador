46b8f507f67a4485e8a7f19fa7c528775dd23e5d
emasaavedra <142761603+emasaavedra@users.noreply.github.com>
2026-08-31 19:09:29 -0400

    Escena Car lista: el auto se mueve de acuerdo a los inputs correctamente

--- godot/scripts/car.gd
+++ godot/scripts/car.gd
@@ -1,11 +1,13 @@
-extends Node3D
+extends VehicleBody3D
+@export var steer_acceleration: float = 3.0
+@export var steer_max: float = 0.5
+@export var engine_power: float = 600.0
+@export var brake_force: float = 300.0
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	var steer_input: float = Input.get_axis("left", "right")
+	var force_input: float = Input.get_axis("backward", "forward")
+	
+	engine_force = force_input * engine_power
+	brake = brake_force if is_zero_approx(force_input) else 0.0
+	steering = move_toward(steering, -steer_input * steer_max, steer_acceleration * delta)
