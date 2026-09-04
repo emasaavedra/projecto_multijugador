4d3edaf0d63a0e41e2c387a7975cc413445be5d0
emasaavedra <142761603+emasaavedra@users.noreply.github.com>
2026-09-03 21:55:49 -0400

    terminada la implementación de RPC, no fue implementado el sync_timer debido a malos resultados

--- godot/scripts/car.gd
+++ godot/scripts/car.gd
@@ -8,12 +8,18 @@ extends VehicleBody3D
 @onready var label_3d: Label3D = $Label3D
 @onready var camera_3d: Camera3D = $SpringArm3D/Camera3D
 @onready var input_synchronizer: InputSynchronizer = $InputSynchronizer
+@onready var sync_timer: Timer = $SyncTimer
 
+#var brake: float
+#var engine_force: float
+#var steering: float
 
 func _ready() -> void:
 	var player_data: Statics.PlayerData = Game.instance.get_player(get_multiplayer_authority())
 	label_3d.text = player_data.name
-	
+	#sync_timer.timeout.connect(_on_sync_timeout)
+	#if is_multiplayer_authority():
+	#	sync_timer.start()
 	
 
 func _input(event: InputEvent) -> void:
@@ -24,8 +30,12 @@ func setup(player_data: Statics.PlayerData) -> void:
 	label_3d.text = player_data.name
 	set_multiplayer_authority(player_data.id)
 	camera_3d.current = is_multiplayer_authority()
+	#if is_multiplayer_authority():
+	#	sync_timer.start()
 	
 func _physics_process(delta: float) -> void:
+	if not is_multiplayer_authority():
+		return
 	var steer_input: float = input_synchronizer.steer_input
 	var force_input: float = input_synchronizer.force_input
 
@@ -34,11 +44,29 @@ func _physics_process(delta: float) -> void:
 	steering = move_toward(steering, -steer_input * steer_max, steer_acceleration * delta)
 	gato._actualizar_animacion(force_input, steer_max)
 	
+	send_data.rpc(global_position, global_rotation, engine_force, brake, steering)
+	
 
 @rpc("any_peer", "call_local", "reliable")
 func test() -> void:
 	Debug.log(name, 10)
 
 @rpc("authority", "call_remote", "unreliable_ordered")
-func send_data(pos: Vector3) -> void:
+func send_data(pos: Vector3, rot: Vector3, engine: float, freno: float, steer_f: float) -> void:
 	global_position = pos
+	global_rotation = rot
+	engine_force = engine
+	brake = freno
+	steering = steer_f
+
+#func _on_sync_timeout() -> void:
+#	_sync.rpc(global_position, global_rotation, brake, engine_force, steering)
+
+#@rpc("reliable")
+#func _sync(pos: Vector3, rot: Vector3, freno: float, engine: float, steer_f: float) -> void:
+#	global_position = global_position.lerp(pos, 0.1)
+#	global_rotation = global_rotation.lerp(rot, 0.1)
+	#brake = brake.lerp(freno, 0.5)
+#	brake = freno
+#	engine_force = engine
+#	steering = steer_f
